87244a7e6b48a25fa044d8466ff829b95c2c719d
SarahFM4 <fmonteiro.sarah@gmail.com>
2026-05-31 14:33:31 -0300

    adicionar lógica de permissões de perfil

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,36 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const autenticar = (req, res, next) => {
+    const token = req.headers['authorization'];
     
+    if (token && token.startsWith('Bearer ')) {
+        const jwtToken = token.slice(7); 
+        jwt.verify(jwtToken, 'SEGREDO', (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ error: 'Token de autenticação inválido.' });
+            }
+            req.usuario = decoded; 
+            next();
+        });
+    } else {
+        res.status(401).json({ error: 'Token de autenticação ausente ou inválido.' });
+    }
+};
 
-}
+const requireBibliotecario = (req, res, next) => {
+    if (req.usuario && req.usuario.perfil === 'bibliotecario') {
+        next();
+    } else {
+        res.status(403).json({ error: 'Acesso negado. Perfil de bibliotecário necessário.' });
+    }
+};
+
+const requireLeitor = (req, res, next) => {
+    if (req.usuario && req.usuario.perfil === 'leitor') {
+        next();
+    } else {
+        res.status(403).json({ error: 'Acesso negado. Perfil de leitor necessário.' });
+    }
+};
+
+module.exports = { autenticar, requireBibliotecario, requireLeitor};
